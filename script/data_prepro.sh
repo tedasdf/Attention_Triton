b3fa2aed0d0f5ad6d7e0a1350540898f85165052
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-03-04 08:10:12 +1100

    Update data_prepro.sh

--- script/data_prepro.sh
+++ script/data_prepro.sh
@@ -13,21 +13,21 @@ dvc pull "$INPUT_PATH" -r "$REMOTE"
 echo "⚙️  Running pipeline..."
 python dataset_pipeline/pipeline.py --config "/home/fypits25/Documents/ted_backyard/actions-runner/_work/Attention_Triton/Attention_Triton/dataset_pipeline/config/pipeline.yaml" --input_dir "$INPUT_PATH" --output_dir "$OUTPUT_PATH"
 
-echo "➕ Tracking output with DVC: $OUTPUT_PATH"
-dvc add "$OUTPUT_PATH"
+# echo "➕ Tracking output with DVC: $OUTPUT_PATH"
+# dvc add "$OUTPUT_PATH"
 
-echo "☁️ Pushing data to S3 via DVC (remote: $REMOTE)"
-dvc push -r "$REMOTE"
+# echo "☁️ Pushing data to S3 via DVC (remote: $REMOTE)"
+# dvc push -r "$REMOTE"
 
-echo "📝 Committing DVC metadata to Git..."
-git add *.dvc .gitignore dvc.yaml dvc.lock
+# echo "📝 Committing DVC metadata to Git..."
+# git add *.dvc .gitignore dvc.yaml dvc.lock
 
-# Only commit if there are staged changes
-if git diff --cached --quiet; then
-  echo "ℹ️  No DVC metadata changes to commit."
-else
-  git commit -m "Data: update processed snapshot ($OUTPUT_PATH)"
-  git push
-fi
+# # Only commit if there are staged changes
+# if git diff --cached --quiet; then
+#   echo "ℹ️  No DVC metadata changes to commit."
+# else
+#   git commit -m "Data: update processed snapshot ($OUTPUT_PATH)"
+#   git push
+# fi
 
-echo "✅ Done."
+# echo "✅ Done."
