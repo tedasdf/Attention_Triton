5e5f1a3466f1c919a703f2d365fcd5c714f9e792
tedasdf <154312357+tedasdf@users.noreply.github.com>
2026-03-09 15:46:39 +1100

    Create sliding.py

---
+++ main/model/attention/sliding.py
@@ -0,0 +1,12 @@
+import torch
+from standard import CausalSelfAttention
+
+
+class SlidingWindowSelfAttention(CausalSelfAttention):
+    def __init__(self, cfg):
+        self.window_size = cfg.window_size
+        super().__init__(cfg)
+
+    def build_mask(self, block_size: int) -> torch.Tensor:
+        full_causal = torch.tril(torch.ones(block_size, block_size))
+        return torch.triu(full_causal, diagonal=-(self.window_size - 1))
